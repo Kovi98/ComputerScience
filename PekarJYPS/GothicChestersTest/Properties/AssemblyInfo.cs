973c826db19a7846d1396f76f1c6096490bd06fd
Kovi98 <pekar.jan98@gmail.com>
2020-11-11 09:50:15 +0100

    Změna struktury - herní logika přesunuta do samostatné knihovny vytvořen projekt pro testování herní knihovny

---
+++ PekarJYPS/GothicChestersTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("GothicChestersTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("GothicChestersTest")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("0514e155-1205-4eae-9c05-2bb0e9c94250")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
